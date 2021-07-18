e5f3fbd85866d25dc028181305b301cbdb8f4dd8
dlschoof <dlschoof@mtu.edu>
2021-07-18 13:50:09 -0400

    Optimized & Added CLI Integration

--- main.py
+++ main.py
@@ -3,19 +3,57 @@
     Store url, title, links in the page, and the parent link in the list.
 '''
 
+from typing import final
 from page import Page
+import sys
 
 
 def main():
-    page_list = []
-
     # Testing Purposes
-    hello = Page(
-        "https://en.wikipedia.org/wiki/Gunilla_Carlsson")
-    hello.populateLinks()
-    hello.getName()
-    for i in hello.links:
-        print(i)
+    try:
+        startingNode = Page(
+            sys.argv[1], 0)
+        startingNode.populateLinks()
+        endNode = Page(sys.argv[2], 0)
+    except:
+        print("Error!")
+
+    result = BFS(startingNode, endNode.url)
+    if result:
+        print("\t"+result)
+    else:
+        print(
+            f"\t{startingNode.getName()} has no links to {endNode.getName()}. Please try again!")
+
+
+def BFS(initialNode, destUrl):
+    queue = [initialNode]
+    visited = []
+
+    while len(queue) > 0:
+        s = queue.pop(0)
+        visited.append(s.url)
+
+        if s.url == destUrl:
+            final_str = s.getName()
+            itr = s.parent
+
+            while itr:
+                final_str = itr.getName() + '--->' + final_str
+                itr = itr.parent
+
+            return final_str
+
+        s.populateLinks()
+
+        if s.depth <= 6:
+            for link in s.links:
+                if link not in visited:
+                    node = Page(link, s.depth + 1)
+                    node.parent = s
+                    queue.append(node)
+
+    return None
 
 
 if __name__ == '__main__':
--- page.py
+++ page.py
@@ -3,21 +3,18 @@ import requests
 
 
 class Page:
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, depth: int) -> None:
         self.url = url
+        self.depth = depth
         self.links = []
-        self.name = None
         self.parent = None
 
-    '''
-        Gets all the links in the page and adds it to the array links
-        In the form of /wiki/..., filtering for several things
-    '''
-
+    # Gets all the links in the page and adds it to the array links
+    # In the form of /wiki/..., filtering for several things
     def populateLinks(self) -> list[str]:
         def checkIfValidLink(link: str) -> bool:
             if link and link.startswith('/wiki/'):
-                if link[6:14] != 'Category':
+                if link[6:14] != 'Category' and link[6:10] != 'File' and link[6:14] != 'Template' and link[6:10] != 'Help' and link[6:15] != 'Wikipedia':
                     return True
             return False
 
@@ -29,16 +26,15 @@ class Page:
         linkArr = [aTag.get('href') for aTag in linkArr]
 
         linkArr = filter(checkIfValidLink, linkArr)
+        linkArr = ['https://en.wikipedia.org' +
+                   wikiTag for wikiTag in linkArr]
         self.links = linkArr
 
-    '''
-        Gets the name of the page from the url
-    '''
-
+    # Gets the name of the page from the url
     def getName(self) -> str:
         response = requests.get(self.url)
         soup = BeautifulSoup(response.text, "html.parser")
 
         titleBox = soup.find(id="firstHeading")
         if titleBox:
-            self.name = titleBox.string
+            return titleBox.text
